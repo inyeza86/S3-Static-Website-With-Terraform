978c6dcb04d8d3e00233d9414790684e4dac2afd
inyeza <inyeza1986@gmail.com>
2026-01-07 17:21:42 -0600

    update records

--- modules/route53/outputs.tf
+++ modules/route53/outputs.tf
@@ -1,5 +1,10 @@
+# output "records" {
+#   description = "The Route53 records created for the CloudFront distribution aliases"
+#   #value       = { for k, v in aws_route53_record.cloudfront : k => v.fqdn }
+#   value = aws_route53_record.cloudfront[*].name
+#}
+
 output "records" {
-  description = "The Route53 records created for the CloudFront distribution aliases"
-  #value       = { for k, v in aws_route53_record.cloudfront : k => v.fqdn }
-  value = aws_route53_record.cloudfront[*].name
+  description = "Route53 record names"
+  value       = [for r in aws_route53_record.cloudfront : r.value.name]
 }
