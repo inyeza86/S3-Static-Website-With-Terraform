3f5d63b649177b5483b4cd8f6e3cb74cde1f6b85
inyeza <inyeza1986@gmail.com>
2026-01-07 16:54:00 -0600

    update

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -2,7 +2,7 @@ output "website_url" {
   description = "The URL of the static website"
   value       = [for a in var.aliases : "https://${a}"]
 }
-output "cloudfront_distribution_id" {
-  description = "The ID of the CloudFront distribution"
-  value       = module.cloudfront_distribution_id
-}
+# output "cloudfront_distribution_id" {
+#   description = "The ID of the CloudFront distribution"
+#   value       = module.cloudfront_distribution_id
+# }
