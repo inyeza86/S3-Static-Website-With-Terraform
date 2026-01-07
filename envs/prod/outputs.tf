c47234b3cf125b823a7ab766713c23f751ed9a6a
inyeza <inyeza1986@gmail.com>
2026-01-07 16:41:44 -0600

    update

--- envs/prod/outputs.tf
+++ envs/prod/outputs.tf
@@ -4,6 +4,6 @@ output "website_url" {
 }
 output "cloudfront_distribution_id" {
   description = "The ID of the CloudFront distribution"
-  value       = aws_cloudfront_distribution.s3_distribution.id
+  value       = module.cloudfront_distribution_id
   #value       = aws_cloudfront_distribution.s3_distribution.id
 }
