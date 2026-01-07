059b13e59b2b2d7dddc0a5ded0f6897ee2063979
inyeza <inyeza1986@gmail.com>
2026-01-07 17:16:27 -0600

    update records

--- modules/route53/outputs.tf
+++ modules/route53/outputs.tf
@@ -1,5 +1,5 @@
 output "records" {
   description = "The Route53 records created for the CloudFront distribution aliases"
   #value       = { for k, v in aws_route53_record.cloudfront : k => v.fqdn }
-  value = aws_route53_record.cloudfront[*].fqdn
+  value = aws_route53_record.cloudfront[*].name
 }
