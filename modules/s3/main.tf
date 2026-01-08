6b7977d2d16bc63f32130a0405a50c3277dcac27
inyeza <inyeza1986@gmail.com>
2026-01-07 19:26:38 -0600

    Update bucket policy

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -47,7 +47,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
         "Resource" : "${aws_s3_bucket.static_website_bucket.arn}/*",
         "Condition" : {
           "StringEquals" : {
-            "AWS:SourceArn" : aws_cloudfront_distribu
+            "AWS:SourceArn" : aws_cloudfront_distribution.s3_distribution.arn
           }
         }
       }
