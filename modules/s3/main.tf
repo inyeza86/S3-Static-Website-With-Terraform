4b2e17e15040a5bc95c6b74d52f9bce5662e27d7
inyeza <inyeza1986@gmail.com>
2026-01-07 19:22:07 -0600

    Added bucket policy

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -28,32 +28,32 @@ resource "aws_cloudfront_origin_access_control" "oac" {
 
 # bucket policy to allow CloudFront access
 
-# resource "aws_s3_bucket_policy" "bucket_policy" {
-#   bucket     = aws_s3_bucket.static_website_bucket.id
-#   depends_on = [aws_s3_bucket_public_access_block.block_public_access]
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket     = aws_s3_bucket.static_website_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.block_public_access]
 
-#   policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Sid" : "AllowCloudFrontServicePrincipalReadOnly",
-#         "Effect" : "Allow",
-#         "Principal" : {
-#           "Service" : "cloudfront.amazonaws.com"
-#         },
-#         "Action" : [
-#           "s3:GetObject",
-#         ],
-#         "Resource" : "${aws_s3_bucket.static_website_bucket.arn}/*",
-#         "Condition" : {
-#           "StringEquals" : {
-#             "AWS:SourceArn" : aws_cloudfront_distribu
-#           }
-#         }
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowCloudFrontServicePrincipalReadOnly",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : [
+          "s3:GetObject",
+        ],
+        "Resource" : "${aws_s3_bucket.static_website_bucket.arn}/*",
+        "Condition" : {
+          "StringEquals" : {
+            "AWS:SourceArn" : aws_cloudfront_distribu
+          }
+        }
+      }
+    ]
+  })
+}
 
 # upload website content to the S3 bucket
 # resource "aws_s3_object" "website_content" {
